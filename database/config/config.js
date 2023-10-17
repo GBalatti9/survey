344400a66760e4326642f5313e43916db7e384a2
GBalatti9 <gas.balatti@gmail.com>
2023-10-17 12:44:56 -0300

    Trying to deploy DB

--- database/config/config.js
+++ database/config/config.js
@@ -17,7 +17,7 @@ module.exports = {
   "production": {
     "username": "root",
     "password": null,
-    "database": "database_production",
+    "database": "trivia_boca",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
